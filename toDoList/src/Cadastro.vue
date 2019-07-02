709b212bebbc0b8bfbe218d9cca5bf27da98ffec
Caioww <sousinhacaio@gmail.com>
2019-07-01 23:04:00 -0300

    mensagem usando diretivas do vue

--- toDoList/src/Cadastro.vue
+++ toDoList/src/Cadastro.vue
@@ -37,31 +37,83 @@
       @botaoAtivado="adicionarPessoa()"
       :confirmacao="true"
     />
+
+    <transition name="fade">
+    <h1 :class="classe" v-if="add">Deu certo!</h1>
+    <h1 v-if="notAdd">Falhou!</h1>
+    </transition>
+    
+    
   </div>
 </template>
 <script>
 import Botao from "./shared/Botao";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
+
+  props:{
+    add:{
+      type: Boolean,
+      default: false
+    },
+    notAdd:{
+      type: Boolean,
+      default: false
+    },
+    classe:{
+      type: String,
+    }
+  },
   components: {
     "meu-botao": Botao
   },
+  created() {
+    this.startInterval()
+},
+
   methods: {
+    startInterval() {
+        setInterval(() => {
+
+                    this.add=false;
+                    this.notAdd = false;
+                    this.classe = "faded"
+                }, 5000)
+      },
+    
     adicionarPessoa() {
-      let pessoa ={};
+      let pessoa = {};
       pessoa.nome = this.$refs.myName.value;
       pessoa.idade = this.$refs.myAge.value;
       pessoa.telefone = this.$refs.myPhone.value;
       pessoa.email = this.$refs.myEmail.value;
-      
-      axios.post("http://localhost:3000/lista",pessoa).then(response => {
-      
-        console.log('Salvo com sucesso');
-    })
+
+      axios
+        .post("http://localhost:3000/lista", pessoa)
+        .then(response => {
+          this.add = true;
+          this.notAdd = false;
+          console.log("Salvo com sucesso");
+        })
+        .catch(error => {
+          if (error.response) {
+            this.notAdd = true;
+            this.add= false;
+          }
+        });
     }
-  }
-}
+    },
+  
+  
+};
 </script>
 <style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active em versões anteriores a 2.1.8 */ {
+  opacity: 0;
+}
 </style>
